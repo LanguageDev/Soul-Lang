c48f3d2746acd778bcd304268a4eddfd74369f24
PlasmaAddict135 <62064430+PlasmaAddict135@users.noreply.github.com>
2021-04-30 16:06:21 -0400

    Remove Comments

--- Program.cs
+++ Program.cs
@@ -14,19 +14,6 @@ namespace SoulLang
                     Right = new NumExpr { Token = new Token(new Position(), TokenType.Integer, "4") },
                 },
             };
-
-            // Ight lets test it
-            // Just change the parser function
-            // Alright, so this is test ready now?
-            // yeah alr this should be okay
-            // works perfectly!
-            // brb in like 2 minutes
-            // I think we're good to start working on the other nodes like "if", and functions
-            // Yeah, lets go add that
-            // Not 100%, you are calling Parse_expr here, don't you want to parse a sequence now?
-            // Yep
-            // ye kk
-            // For if we will need a bool value in the runtime
             while (true)
             {
                 var input = Console.ReadLine();
